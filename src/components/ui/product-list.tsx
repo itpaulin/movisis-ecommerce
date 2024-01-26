cf05819f98da70630e4cb894114f01bca89035c9
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-26 14:31:32 -0300

    feat: responsive list product

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -99,7 +99,7 @@ const ProductList = ({ products }: ProductListProps) => {
           />
         </div>
       )}
-      <div className="grid grid-cols-2 gap-3 pt-4 md:grid-cols-6">
+      <div className="grid grid-cols-2 gap-3 pt-4 md:grid-cols-4 lg:grid-cols-6">
         {filteredProducts.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
