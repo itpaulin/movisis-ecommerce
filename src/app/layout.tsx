0d22a43750d4882564230011ab9b84f1e4ad2a99
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-25 23:10:27 -0300

    feat: add top title to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/header";
+import Header from "@/components/ui/header";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,16 @@
 import { ShoppingCart } from "lucide-react";
-import Cart from "./ui/cart";
-import { ModeToggle } from "./ui/mode-toggle";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { Button } from "./ui/button";
+import Cart from "./cart";
+import { ModeToggle } from "./mode-toggle";
+import { Sheet, SheetContent, SheetTrigger } from "./sheet";
+import { Button } from "./button";
 
 const Header = () => {
   return (
-    <div className="flex text-center justify-between">
+    <div className="flex text-center justify-between mb-10 pt-7 px-6">
       <ModeToggle />
-      <h1 className="font-bold text-3xl text-primary">Movisis Ecommerce</h1>
+      <h1 className="font-semibold text-lg ">
+        <span className="text-primary">Movisis</span> Ecommerce
+      </h1>
       <Sheet>
         <SheetTrigger asChild>
           <Button size="icon" variant="outline">
